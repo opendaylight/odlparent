445b7c12de990324a6a788e14656b1fe77cd0236
Vratko Polak <vrpolak@cisco.com>
2016-07-20 16:49:35 +0200

    Increase -Xmx in SingleFeatureTest to 3GB
    In Boron, odl-integration-all may not fit into 2GB Karaf.
    
    Change-Id: I73853578018b8b2173b5b98d18a855b15f734182
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- features-test/src/main/java/org/opendaylight/odlparent/featuretest/SingleFeatureTest.java
+++ features-test/src/main/java/org/opendaylight/odlparent/featuretest/SingleFeatureTest.java
@@ -116,7 +116,7 @@ public class SingleFeatureTest {
     public Option[] config() throws IOException {
         return new Option[] {
             // TODO: Find a way to inherit memory limits from Maven options.
-            new VMOption("-Xmx2g"),
+            new VMOption("-Xmx=3g"),
             new VMOption("-XX:MaxPermSize=512m"),
             getKarafDistroOption(),
             when(Boolean.getBoolean(KEEP_UNPACK_DIRECTORY_PROP)).useOptions(keepRuntimeFolder()),
