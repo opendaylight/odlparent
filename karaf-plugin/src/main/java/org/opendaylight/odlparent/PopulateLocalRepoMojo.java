e3a66683bef02c1ee7890681ab4db7e27ea8890c
Vratko Polak <vrpolak@cisco.com>
2016-08-01 15:22:41 +0200

    Bug 6187: Force more maven resolution in karaf-plugin
    Especially concerning level 1 feature repository dependencies.
    This fixed Bug 6187 in my local testing.
    
    + Improve logging.
    
    Change-Id: Ie973e3669510061afd59a6c9d22f7a9161f0bd13
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- karaf-plugin/src/main/java/org/opendaylight/odlparent/PopulateLocalRepoMojo.java
+++ karaf-plugin/src/main/java/org/opendaylight/odlparent/PopulateLocalRepoMojo.java
@@ -26,7 +26,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
-
 import org.apache.karaf.features.internal.model.Features;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -115,16 +114,17 @@ public class PopulateLocalRepoMojo
                     aetherUtil.resolveDependencies(MvnToAetherMapper.toAether(project.getDependencies()),
                             new KarafFeaturesDependencyFilter()));
             Set<Features> features = FeatureUtil.readFeatures(featureArtifacts);
-            features.addAll(FeatureUtil.findAllFeaturesRecursively(aetherUtil, features,
-                    FeatureUtil.featuresRepositoryToCoords(features)));
+            // Do not provide FeatureUtil.featuresRepositoryToCoords(features)) as existingCoords
+            // to findAllFeaturesRecursively, as those coords are not resolved yet, and it would lead to Bug 6187.
+            features.addAll(FeatureUtil.findAllFeaturesRecursively(aetherUtil, features));
             for (Features feature : features) {
-                LOG.info("Features Repos  discovered recursively: {}", feature.getName());
+                LOG.info("Feature repository discovered recursively: {}", feature.getName());
             }
             Set<Artifact> artifacts = aetherUtil.resolveArtifacts(FeatureUtil.featuresToCoords(features));
             artifacts.addAll(featureArtifacts);
 
             for (Artifact artifact : artifacts) {
-                LOG.info("Artifacts to be installed: {}", artifact.toString());
+                LOG.debug("Artifact to be installed: {}", artifact.toString());
             }
             if (localRepo != null) {
                 aetherUtil.installArtifacts(artifacts);
