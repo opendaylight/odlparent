8a48371ebc1436caa1e377b0da5a63d82e07fd6d
Stephen Kitt <skitt@redhat.com>
2016-12-21 15:27:21 +0100

    BUG-7473: add Karaf 4 features
    Instead of switching wholesale to Karaf 4 only, this patch adds
    duplicate features for Karaf 4:
    
    * feature-repo-parent is the new parent for Karaf 4 feature
      repositories (the Karaf 3 parent is now feature3-repo-parent, used
      only in odlparent);
    * single-feature-parent is the new parent for Karaf 4 features (the
      Karaf 3 parent is now single-feature3-parent, used only in
      odlparent);
    * features4-akka is the new odlparent Karaf 4 feature repository for
      Akka (dropping the old, backwards-compatible features);
    * features4-odlparent is the new odlparent Karaf 4 feature repository
      for all other features (again, dropping the old,
      backwards-compatible features).
    
    features-parent isn't duplicated for Karaf 4, downstreams are
    encouraged to use at least feature-repo-parent instead.
    
    This patch includes the following changes too:
    * bundle uninstall failures are ignored in SingleFeatureTest;
    * the deprecated features are reduced to delegates for their
      replacement features (e.g. odl-akka-scala simply pulls in
      odl-akka-scala-2.11);
    * karaf4-parent only pulls in standard and war (these two features
      include all the other features that were explicitly listed);
    * Pax Exam is upgraded to 4.9.2 (necessary to build with Karaf 4, but
      backwards-compatible with Karaf 3);
    * Jacoco is upgraded to 0.7.7.201606060606 (as above);
    * pax-url is upgraded to 2.5.1 (as above);
    * the Protobuf tweaking for clustering is no longer necessary and is
      removed;
    * bundles-test needs wrap as a pre-requisite, which requires wrapping
      it in a feature (odl-bundles-test) and using that in the Karaf 4
      SingleFeatureTest.
    
    Change-Id: I251aa7aefe6d3d7f93a73b5256bad6e596147c66
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ bundles4-test/src/main/java/org/opendaylight/odlparent/bundlestest/BundleDiagInfos.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.bundlestest;
+
+import static org.apache.karaf.bundle.core.BundleState.Active;
+import static org.apache.karaf.bundle.core.BundleState.Installed;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.karaf.bundle.core.BundleInfo;
+import org.apache.karaf.bundle.core.BundleService;
+import org.apache.karaf.bundle.core.BundleState;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+/**
+ * System readiness diagnostic summary information.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class BundleDiagInfos {
+
+    private final List<String> okBundleStateInfoTexts;
+    private final List<String> nokBundleStateInfoTexts;
+    private final List<String> whitelistedBundleStateInfoTexts;
+    private final Map<BundleState, Integer> bundleStatesCounters;
+
+    private static final Map<String, BundleState> WHITELISTED_BUNDLES = ImmutableMap.of(
+            "slf4j.log4j12", Installed );
+
+    public static BundleDiagInfos forContext(BundleContext bundleContext, BundleService bundleService) {
+        List<String> okBundleStateInfoTexts = new ArrayList<>();
+        List<String> nokBundleStateInfoTexts = new ArrayList<>();
+        List<String> whitelistedBundleStateInfoTexts = new ArrayList<>();
+        Map<BundleState, Integer> bundleStatesCounters = new EnumMap<>(BundleState.class);
+        for (BundleState bundleState : BundleState.values()) {
+            bundleStatesCounters.put(bundleState, 0);
+        }
+
+        for (Bundle bundle : bundleContext.getBundles()) {
+            String bundleSymbolicName = bundle.getSymbolicName();
+            BundleInfo karafBundleInfo = bundleService.getInfo(bundle);
+            BundleState karafBundleState = karafBundleInfo.getState();
+
+            String bundleStateDiagText = "OSGi state = " + bundleStatetoText(bundle.getState())
+                + ", Karaf bundleState = " + karafBundleState;
+            String diagText = bundleService.getDiag(bundle);
+            if (!Strings.isNullOrEmpty(diagText)) {
+                bundleStateDiagText += ", due to: " + diagText;
+            }
+
+            if (WHITELISTED_BUNDLES.get(bundleSymbolicName) != null) {
+                if (WHITELISTED_BUNDLES.get(bundleSymbolicName).equals(karafBundleState)) {
+                    String msg = "WHITELISTED " + bundleSymbolicName + ": " + bundleStateDiagText;
+                    whitelistedBundleStateInfoTexts.add(msg);
+                    continue;
+                }
+            }
+
+            bundleStatesCounters.compute(karafBundleState, (key, counter) -> counter + 1);
+
+            // BundleState comparison as in Karaf's "diag" command,
+            // see https://github.com/apache/karaf/blob/master/bundle/core/src/main/java/org/apache/karaf/bundle/command/Diag.java
+            // but we intentionally, got a little further than Karaf's "diag" command,
+            // and instead of only checking some states, we check what's really Active,
+            // but accept that some remain just Resolved:
+            if (karafBundleState != Active && !(karafBundleState == BundleState.Resolved)) {
+                String msg = "NOK " + bundleSymbolicName + ": " + bundleStateDiagText;
+                nokBundleStateInfoTexts.add(msg);
+            } else {
+                String msg = "OK " + bundleSymbolicName + ": " + bundleStateDiagText;
+                okBundleStateInfoTexts.add(msg);
+            }
+        }
+
+        return new BundleDiagInfos(okBundleStateInfoTexts, nokBundleStateInfoTexts,
+                whitelistedBundleStateInfoTexts, bundleStatesCounters);
+    }
+
+    private static String bundleStatetoText(int state) {
+        switch (state) {
+            case Bundle.INSTALLED:
+                return "Installed";
+            case Bundle.RESOLVED:
+                return "Resolved";
+            case Bundle.STARTING:
+                return "Starting";
+            case Bundle.ACTIVE:
+                return "Active";
+            case Bundle.STOPPING:
+                return "Stopping";
+            case Bundle.UNINSTALLED:
+                return "Uninstalled";
+            default:
+                return state + "???";
+        }
+    }
+
+    private BundleDiagInfos(List<String> okBundleStateInfoTexts, List<String> nokBundleStateInfoTexts,
+            List<String> whitelistedBundleStateInfoTexts, Map<BundleState, Integer> bundleStatesCounters) {
+        this.okBundleStateInfoTexts = ImmutableList.copyOf(okBundleStateInfoTexts);
+        this.nokBundleStateInfoTexts = ImmutableList.copyOf(nokBundleStateInfoTexts);
+        this.whitelistedBundleStateInfoTexts = ImmutableList.copyOf(whitelistedBundleStateInfoTexts);
+        this.bundleStatesCounters = ImmutableMap.copyOf(bundleStatesCounters);
+    }
+
+    public SystemState getSystemState() {
+        if (bundleStatesCounters.get(BundleState.Failure) > 0) {
+            return SystemState.Failure;
+        } else if (bundleStatesCounters.get(BundleState.Stopping) > 0) {
+            return SystemState.Stopping;
+        } else if (bundleStatesCounters.get(BundleState.Installed) == 0
+                // No, just Resolved is OK, so do not: && bundleStatesCounters.get(BundleState.Resolved) == 0
+                && bundleStatesCounters.get(BundleState.Unknown) == 0
+                && bundleStatesCounters.get(BundleState.GracePeriod) == 0
+                && bundleStatesCounters.get(BundleState.Waiting) == 0
+                && bundleStatesCounters.get(BundleState.Starting) == 0
+                // BundleState.Active *should* be ~= total # of bundles (minus Resolved, and whitelisted installed)
+                && bundleStatesCounters.get(BundleState.Stopping) == 0
+                && bundleStatesCounters.get(BundleState.Failure) == 0) {
+            return SystemState.Active;
+        } else {
+            return SystemState.Booting;
+        }
+    }
+
+    public String getFullDiagnosticText() {
+        StringBuilder sb = new StringBuilder(getSummaryText());
+        int failureNumber = 1;
+        for (String nokBundleStateInfoText : getNokBundleStateInfoTexts()) {
+            sb.append('\n');
+            sb.append(failureNumber++);
+            sb.append(". ");
+            sb.append(nokBundleStateInfoText);
+        }
+        return sb.toString();
+    }
+
+    public String getSummaryText() {
+        return "diag: " + getSystemState() + " " + bundleStatesCounters.toString();
+    }
+
+    public List<String> getNokBundleStateInfoTexts() {
+        return ImmutableList.copyOf(nokBundleStateInfoTexts);
+    }
+
+    public List<String> getOkBundleStateInfoTexts() {
+        return ImmutableList.copyOf(okBundleStateInfoTexts);
+    }
+
+    public List<String> getWhitelistedBundleStateInfoTexts() {
+        return ImmutableList.copyOf(whitelistedBundleStateInfoTexts);
+    }
+
+    @Override
+    public String toString() {
+        return getFullDiagnosticText();
+    }
+}
