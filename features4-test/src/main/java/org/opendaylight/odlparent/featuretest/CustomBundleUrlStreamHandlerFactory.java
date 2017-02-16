e0f4e8639cdabc79026015096e6eb5700423adf4
Robert Varga <rovarga@cisco.com>
2017-02-16 18:35:29 +0100

    BUG-7813: remove spring from base distribution
    We do not use spring anywhere in the project, hence we can safely
    remove it from the distribution, cutting it down by 35MB.
    
    Change-Id: I07e6c8f0996d0d9cc9b0d6c50195a9fbb9f448b7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- features4-test/src/main/java/org/opendaylight/odlparent/featuretest/CustomBundleUrlStreamHandlerFactory.java
+++ features4-test/src/main/java/org/opendaylight/odlparent/featuretest/CustomBundleUrlStreamHandlerFactory.java
@@ -26,14 +26,12 @@ import java.net.URLStreamHandlerFactory;
 
 import org.apache.karaf.deployer.blueprint.BlueprintURLHandler;
 import org.apache.karaf.deployer.features.FeatureURLHandler;
-import org.apache.karaf.deployer.spring.SpringURLHandler;
 
 public class CustomBundleUrlStreamHandlerFactory implements URLStreamHandlerFactory {
 
     private static final String MVN_URI_PREFIX = "mvn";
     private static final String WRAP_URI_PREFIX = "wrap";
     private static final String FEATURE_URI_PREFIX = "feature";
-    private static final String SPRING_URI_PREFIX = "spring";
     private static final String BLUEPRINT_URI_PREFIX = "blueprint";
 
     @Override
@@ -45,8 +43,6 @@ public class CustomBundleUrlStreamHandlerFactory implements URLStreamHandlerFact
                 return new org.ops4j.pax.url.wrap.Handler();
             case FEATURE_URI_PREFIX:
                 return new FeatureURLHandler();
-            case SPRING_URI_PREFIX:
-                return new SpringURLHandler();
             case BLUEPRINT_URI_PREFIX:
                 return new BlueprintURLHandler();
             default:
