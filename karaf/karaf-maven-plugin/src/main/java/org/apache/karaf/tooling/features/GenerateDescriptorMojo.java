a4d9f3024cd25aaded9bccb04d652fe943fafccc
Stephen Kitt <skitt@redhat.com>
2017-02-17 14:29:19 +0100

    Generator: merge existing dependency information
    This patch allows the generator to merge dependency information from
    feature.xml stubs and POMs ("dependency" and "prerequisite" flags).
    This allows version information to be pulled from the POMs and still
    allow the flags to be set.
    
    Bug: 7752
    Bug: 7753
    Change-Id: I2b5147dc1636b4c9a3ec3b77654c9994702c7877
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- karaf/karaf-maven-plugin/src/main/java/org/apache/karaf/tooling/features/GenerateDescriptorMojo.java
+++ karaf/karaf-maven-plugin/src/main/java/org/apache/karaf/tooling/features/GenerateDescriptorMojo.java
@@ -537,7 +537,14 @@ public class GenerateDescriptorMojo extends MojoSupport {
             }
             for (Feature includedFeature : includedFeatures.getFeature()) {
                 Dependency dependency = new Dependency(includedFeature.getName(), includedFeature.getVersion());
-                // We musn't de-duplicate here, we may have seen a feature in !add mode
+                // Determine what dependency we're actually going to use
+                Dependency matchingDependency = findMatchingDependency(feature.getFeature(), dependency);
+                if (matchingDependency != null) {
+                    // The feature already has a matching dependency, merge them
+                    mergeDependencies(matchingDependency, dependency);
+                    dependency = matchingDependency;
+                }
+                // We mustn't de-duplicate here, we may have seen a feature in !add mode
                 otherFeatures.put(dependency, includedFeature);
                 if (add) {
                     if (!feature.getFeature().contains(dependency)) {
@@ -552,6 +559,27 @@ public class GenerateDescriptorMojo extends MojoSupport {
                             this.dependencyHelper.artifactToMvn(artifact, getVersionOrRange(parent, artifact)));
                 }
             }
+        }
+    }
+
+    private Dependency findMatchingDependency(List<Dependency> dependencies, Dependency reference) {
+        for (Dependency dependency : dependencies) {
+            if (dependency.getName().equals(reference.getName())) {
+                return dependency;
+            }
+        }
+        return null;
+    }
+
+    private void mergeDependencies(Dependency target, Dependency source) {
+        if (target.getVersion() == null || Feature.DEFAULT_VERSION.equals(target.getVersion())) {
+            target.setVersion(source.getVersion());
+        }
+        if (source.isDependency()) {
+            target.setDependency(true);
+        }
+        if (source.isPrerequisite()) {
+            target.setPrerequisite(true);
         }
     }
 
