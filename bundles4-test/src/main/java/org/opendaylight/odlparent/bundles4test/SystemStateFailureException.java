7c3b7c22de69f65f384598d6810f6ce3edd51348
Michael Vorburger <vorburger@redhat.com>
2017-05-11 01:55:40 +0200

    TestBundleDiag API surface improved
    required for https://git.opendaylight.org/gerrit/#/c/56749/
    
    The implicit throws org.awaitility.core.ConditionTimeoutException and
    java.lang.AssertionError were not great.. an AssertionError is special,
    and more importantly Awaitility's ConditionTimeoutException is an
    implementation detail.
    
    Awaitility is actually bundled as a private OSGi embedded JAR-in-JAR,
    which is a bit like shaded fat über JARs with relocation in non-OSGi
    Java built by the maven-shade-plugin or maven-assembly-plugin; so it was
    wrong to throw an exception from it to a consumer of this utility.
    
    Therefore, this introduces a new SystemStateFailureException instead.
    
    Change-Id: I7b48f5d39258a62c38a3fb8c98a6a35b97360276
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ bundles4-test/src/main/java/org/opendaylight/odlparent/bundles4test/SystemStateFailureException.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.bundles4test;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+
+/**
+ * Exception thrown (only) by
+ * {@link TestBundleDiag#checkBundleDiagInfos(org.osgi.framework.BundleContext, long, java.util.concurrent.TimeUnit)}
+ * if there are any OSGi bundles that failed to start.  This is based on not only {@link Bundle#getState()}'s
+ * {@link Bundle#ACTIVE}, but also DI wiring systems such as blueprint containers.  The Exceptions' message
+ * will likely contain a longer multi-line String with extensive technical details including all failed
+ * bundles' states, detailed technical information related to OSGi bundle and blueprint resolution,
+ * and possibly exceptions incl. stack traces thrown by {@link BundleActivator} start() methods and
+ * dependency injection object wiring {@literal @}PostConstruct "init" type methods.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class SystemStateFailureException extends Exception {
+    private static final long serialVersionUID = 2024307948995818320L;
+
+    private final BundleDiagInfos bundleDiagInfos;
+
+    public SystemStateFailureException(String message, Throwable cause, BundleDiagInfos bundleDiagInfos) {
+        super(message, cause);
+        this.bundleDiagInfos = bundleDiagInfos;
+    }
+
+    public SystemStateFailureException(String message, BundleDiagInfos bundleDiagInfos) {
+        super(message);
+        this.bundleDiagInfos = bundleDiagInfos;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage() + "\n" + bundleDiagInfos.getFullDiagnosticText();
+    }
+
+    public BundleDiagInfos getBundleDiagInfos() {
+        return bundleDiagInfos;
+    }
+}
