64bec352b80d529e2c0909a0d1416f9402881f7e
Michael Vorburger <vorburger@redhat.com>
2017-05-11 01:55:40 +0200

    Bug 8415 related: TestBundleDiag API improved
    required for https://git.opendaylight.org/gerrit/#/c/56749/
    
    The implicit throws org.awaitility.core.ConditionTimeoutException and
    java.lang.AssertionError were not great.. an AssertionError is special,
    and more importantly Awaitility's ConditionTimeoutException is an
    implementation detail.
    
    Awaitility is actually bundled as a private OSGi embedded JAR-in-JAR,
    which is a bit like shaded fat über JARs with relocation in non-OSGi
    Java built by the maven-shade-plugin or maven-assembly-plugin; so it was
    wrong to throw an exception from it to a consumer of this utility.
    
    Therefore, this introduces a new SystemStateFailureException instead.
    
    Change-Id: I7b48f5d39258a62c38a3fb8c98a6a35b97360276
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- bundles4-test/src/main/java/org/opendaylight/odlparent/bundles4test/TestBundleDiag.java
+++ bundles4-test/src/main/java/org/opendaylight/odlparent/bundles4test/TestBundleDiag.java
@@ -44,17 +44,9 @@ public class TestBundleDiag {
         bundleContext.ungetService(bundleServiceReference);
     }
 
-    public static void checkBundleDiagInfos(BundleContext bundleContext, long timeout, TimeUnit timeoutUnit) {
-        TestBundleDiag diag = new TestBundleDiag(bundleContext);
-        try {
-            diag.checkBundleDiagInfos(timeout, timeoutUnit);
-        } finally {
-            diag.close();
-        }
-    }
-
     /**
-     * Does the equivalent of the "diag" CLI command, and fails the test if anything incl. bundle wiring is NOK.
+     * Does something similar to Karaf's "diag" CLI command,
+     * and throws a {@link SystemStateFailureException} if anything incl. bundle wiring is NOK.
      *
      * <p>The implementation is based on Karaf's BundleService, and not the BundleStateService,
      * because each Karaf supported DI system (such as Blueprint and Declarative Services, see String constants
@@ -63,7 +55,17 @@ public class TestBundleDiag {
      *
      * @author Michael Vorburger, based on guidance from Christian Schneider
      */
-    private void checkBundleDiagInfos(long timeout, TimeUnit timeoutUnit) {
+    public static void checkBundleDiagInfos(BundleContext bundleContext, long timeout, TimeUnit timeoutUnit)
+            throws SystemStateFailureException {
+        TestBundleDiag diag = new TestBundleDiag(bundleContext);
+        try {
+            diag.checkBundleDiagInfos(timeout, timeoutUnit);
+        } finally {
+            diag.close();
+        }
+    }
+
+    private void checkBundleDiagInfos(long timeout, TimeUnit timeoutUnit) throws SystemStateFailureException {
         LOG.info("checkBundleDiagInfos() started...");
         try {
             Awaitility.await("checkBundleDiagInfos")
@@ -83,7 +85,7 @@ public class TestBundleDiag {
                 LOG.error("diag failure; BundleService reports bundle(s) which failed or are already stopping"
                         + " (details in following INFO and ERROR log messages...)");
                 logBundleDiagInfos(bundleInfos);
-                throw new AssertionError(bundleInfos.getFullDiagnosticText());
+                throw new SystemStateFailureException("diag failed; some bundles failed to start", bundleInfos);
 
             } else {
                 // Inform the developer of the green SystemState.Active
@@ -97,7 +99,7 @@ public class TestBundleDiag {
                     + " (details in following INFO and ERROR log messages...)");
             BundleDiagInfos bundleInfos = getBundleDiagInfos();
             logBundleDiagInfos(bundleInfos);
-            throw e; // fail the test!
+            throw new SystemStateFailureException("diag timeout; some bundles are still not active:", e, bundleInfos);
         }
     }
 
