c884bfefc9f984acff702bea94362ef7958d7ea9
Michael Vorburger <vorburger@redhat.com>
2017-05-10 02:23:31 +0200

    Bug 8415 related: DiagUpdatesListener
    to allow TestBundleDiag to feedback async status to callers
    
    required for https://git.opendaylight.org/gerrit/#/c/56749/
    
    Change-Id: I39b6f15a6aff0f8685246f4517aa35aad30064a4
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ bundles4-test/src/main/java/org/opendaylight/odlparent/bundles4test/DiagUpdatesListener.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.bundles4test;
+
+/**
+ * Listener which can be notified for every diag update.
+ *
+ * @author Michael Vorburger.ch
+ */
+@FunctionalInterface
+public interface DiagUpdatesListener {
+
+    void onUpdate(BundleDiagInfos bundleDiagInfos, long elapsedTimeInMS, long remainingTimeInMS);
+
+}
