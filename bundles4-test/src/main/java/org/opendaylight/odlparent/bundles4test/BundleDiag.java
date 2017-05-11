0d0597206ef29e13c5369542a20bdb8e66384864
Michael Vorburger <vorburger@redhat.com>
2017-05-11 14:03:48 +0200

    Bug 8415 related: TestBundleDiag as service (non-static)
    required for https://git.opendaylight.org/gerrit/#/c/56749/
    
    It's not urgently required immediately, but will be when the new ready
    service will have to eventually be useable in non-OSGi environments such
    as component tests (in which case an alternative implementation of the
    API interface can be stubbed).
    
    Change-Id: I3f3a5037ea479b0fa8b8356b1470f4db595ccd89
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ bundles4-test/src/main/java/org/opendaylight/odlparent/bundles4test/BundleDiag.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.bundles4test;
+
+/**
+ * Obtain OSGi bundles' diagnostics.
+ *
+ * @author Michael Vorburger.ch
+ */
+public interface BundleDiag {
+
+}
