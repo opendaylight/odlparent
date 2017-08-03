4bdcca5dbd47905d61fcb5ffb893ac3efce1d337
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-08-03 17:40:41 -0400

    Add SPDX License header
    As defined by https://spdx.org/using-spdx
    
    This bash script can be used to update spdx headers in projects
    quickly:
    
    find . -name "*.java" | xargs sed -i '/Copyright/i \
     * SPDX-License-Identifier: EPL-1.0\
     *'
    
    Change-Id: I8d87c2bdf30f17e16bbbcb3bf730e53891221ae3
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- karaf-plugin/src/main/java/org/opendaylight/odlparent/MvnToAetherMapper.java
+++ karaf-plugin/src/main/java/org/opendaylight/odlparent/MvnToAetherMapper.java
@@ -1,4 +1,6 @@
 /*
+ * SPDX-License-Identifier: EPL-1.0
+ *
  * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
