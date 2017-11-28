2341a164090add776f955deacd2290454a013c66
Michael Vorburger <vorburger@redhat.com>
2017-11-28 18:36:41 +0100

    TestBundleDiag with configurable await logging
    Allows infrautils.ready to make TestBundleDiag INFO log less verbose
    
    The full list of NOK bundles during boot caused confusion, even if INFO
    
    see https://jira.opendaylight.org/browse/INFRAUTILS-17
    
    Change-Id: Iad1badb29e6a061812640a9182973d7c2b679836
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ bundles-test-lib/src/main/java/org/opendaylight/odlparent/bundlestest/lib/TimeInfo.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.bundlestest.lib;
+
+/**
+ * Value Object for elapsed and remaining time.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class TimeInfo {
+
+    private final long elapsedTimeInMS;
+    private final long remainingTimeInMS;
+
+    public TimeInfo(long elapsedTimeInMS, long remainingTimeInMS) {
+        this.elapsedTimeInMS = elapsedTimeInMS;
+        this.remainingTimeInMS = remainingTimeInMS;
+    }
+
+    public long getElapsedTimeInMS() {
+        return elapsedTimeInMS;
+    }
+
+    public long getRemainingTimeInMS() {
+        return remainingTimeInMS;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (elapsedTimeInMS ^ elapsedTimeInMS >>> 32);
+        result = prime * result + (int) (remainingTimeInMS ^ remainingTimeInMS >>> 32);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof TimeInfo)) {
+            return false;
+        }
+        TimeInfo other = (TimeInfo) obj;
+        if (elapsedTimeInMS != other.elapsedTimeInMS) {
+            return false;
+        }
+        if (remainingTimeInMS != other.remainingTimeInMS) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeInfo [elapsedTimeInMS=" + elapsedTimeInMS + ", remainingTimeInMS=" + remainingTimeInMS + "]";
+    }
+
+}
