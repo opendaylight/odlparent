704ab368fc12e73c1d1a6ef086f5dd2443c37e00
Robert Varga <robert.varga@pantheon.tech>
2019-01-01 23:12:21 +0100

    Clean up karaf-4.2.2/JDK9 options
    This reworks the workaround to use OptionUtils and
    dynamically-determined karafVersion.
    
    Change-Id: I5b46a5b8dd5aed42aeebcb5d7ae2c8238c2a0167
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- features-test/src/main/java/org/opendaylight/odlparent/featuretest/SingleFeatureTest.java
+++ features-test/src/main/java/org/opendaylight/odlparent/featuretest/SingleFeatureTest.java
@@ -44,7 +44,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opendaylight.odlparent.bundlestest.lib.TestBundleDiag;
 import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.MavenUtils;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.OptionUtils;
 import org.ops4j.pax.exam.ProbeBuilder;
 import org.ops4j.pax.exam.TestProbeBuilder;
 import org.ops4j.pax.exam.karaf.container.internal.JavaVersionUtil;
@@ -116,35 +118,6 @@ public class SingleFeatureTest {
             + "http://repository.springsource.com/maven/bundles/release@id=spring.ebr.release, "
             + "http://repository.springsource.com/maven/bundles/external@id=spring.ebr.external, "
             + "http://zodiac.springsource.com/maven/bundles/release@id=gemini ";
-
-    private static final String KARAF_VERSION = System.getProperty("karaf.version", "4.2.2");
-
-    private static final VMOption[] JDK9PLUS_VMOPIONS = new VMOption[] {
-        new VMOption("--add-reads=java.xml=java.logging"),
-        new VMOption("--add-exports=java.base/org.apache.karaf.specs.locator=java.xml,ALL-UNNAMED"),
-        new VMOption("--patch-module"),
-        new VMOption("java.base=lib/endorsed/org.apache.karaf.specs.locator-" + KARAF_VERSION + ".jar"),
-        new VMOption("--patch-module"),
-        new VMOption("java.xml=lib/endorsed/org.apache.karaf.specs.java.xml-" + KARAF_VERSION + ".jar"),
-        new VMOption("--add-opens"),
-        new VMOption("java.base/java.security=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.base/java.net=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.base/java.lang=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.base/java.util=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.naming/javax.naming.spi=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED"),
-        new VMOption("--add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED"),
-        new VMOption("--add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED"),
-        new VMOption("--add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED"),
-        new VMOption("--add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED"),
-        new VMOption("-classpath"),
-        new VMOption("lib/jdk9plus/*" + File.pathSeparator + "lib/boot/*")
-    };
 
     @Inject @NonNull
     private BundleContext bundleContext;
@@ -265,9 +238,33 @@ public class SingleFeatureTest {
             return baseConfig;
         }
 
-        final Option[] jdk9plus = Arrays.copyOf(baseConfig, baseConfig.length + JDK9PLUS_VMOPIONS.length);
-        System.arraycopy(JDK9PLUS_VMOPIONS, 0, jdk9plus, baseConfig.length, JDK9PLUS_VMOPIONS.length);
-        return jdk9plus;
+        final String karafVersion = MavenUtils.getArtifactVersion("org.apache.karaf", "apache-karaf");
+        return OptionUtils.combine(baseConfig, new VMOption[] {
+            new VMOption("--add-reads=java.xml=java.logging"),
+            new VMOption("--add-exports=java.base/org.apache.karaf.specs.locator=java.xml,ALL-UNNAMED"),
+            new VMOption("--patch-module"),
+            new VMOption("java.base=lib/endorsed/org.apache.karaf.specs.locator-" + karafVersion + ".jar"),
+            new VMOption("--patch-module"),
+            new VMOption("java.xml=lib/endorsed/org.apache.karaf.specs.java.xml-" + karafVersion + ".jar"),
+            new VMOption("--add-opens"),
+            new VMOption("java.base/java.security=ALL-UNNAMED"),
+            new VMOption("--add-opens"),
+            new VMOption("java.base/java.net=ALL-UNNAMED"),
+            new VMOption("--add-opens"),
+            new VMOption("java.base/java.lang=ALL-UNNAMED"),
+            new VMOption("--add-opens"),
+            new VMOption("java.base/java.util=ALL-UNNAMED"),
+            new VMOption("--add-opens"),
+            new VMOption("java.naming/javax.naming.spi=ALL-UNNAMED"),
+            new VMOption("--add-opens"),
+            new VMOption("java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED"),
+            new VMOption("--add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED"),
+            new VMOption("--add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED"),
+            new VMOption("--add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED"),
+            new VMOption("--add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED"),
+            new VMOption("-classpath"),
+            new VMOption("lib/jdk9plus/*" + File.pathSeparator + "lib/boot/*")
+        });
     }
 
     private static String getNewJFRFile() throws IOException {
