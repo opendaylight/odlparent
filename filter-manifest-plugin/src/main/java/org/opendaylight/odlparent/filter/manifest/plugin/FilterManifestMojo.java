9839c6fdc13479994043645f485343c69c5e133c
Robert Varga <robert.varga@pantheon.tech>
2019-01-02 18:30:05 +0100

    WIP: rework BouncyCastle/OSGi integration
    We are packaging BC jars twice, once in lib/boot for the purposes
    of having the provider set by karaf's Main class and second time
    to expose them to OSGi world via startup.properties as normal bundles.
    
    This is required because the boot classpath is not exposed directly
    into OSGi world and thus bundles linking to BC would not be able
    to find correct exports.
    
    An alternative would be to specify all the packages through
    org.osgi.framework.system.packages.extra, but that has the downside
    of being static and requiring maintentance.
    
    This patch takes the route of Framework Extension Bundles, where
    it provides two fragment bundles, which attach to system.bundle,
    which itself has access to the boot class loader -- hence they
    can themselves contain appropriate Export-Package instructions.
    
    We generate these instructions from upstream bundles by unpacking
    their manifest and retaining only Export-Package instruction (via a
    simplistic maven plugin) and feeding this as input into
    maven-bundle-plugin.
    
    JIRA: ODLPARENT-185
    Change-Id: I5cbfc9a499aa117581004585f4fa5283f65a5a38
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ filter-manifest-plugin/src/main/java/org/opendaylight/odlparent/filter/manifest/plugin/FilterManifestMojo.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2019 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.filter.manifest.plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.jar.Attributes;
+import java.util.jar.Attributes.Name;
+import java.util.jar.Manifest;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Mojo processing filtering an input Manifest file into an output, retaining only selected entries.
+ */
+@Mojo(name = "filter-manifest")
+public class FilterManifestMojo extends AbstractMojo {
+    private static final Logger LOG = LoggerFactory.getLogger(FilterManifestMojo.class);
+
+    @Parameter(required = true)
+    private File inputFile;
+
+    @Parameter(required = true)
+    private File outputFile;
+
+    @Parameter(required = true)
+    private List<String> retainedAttributes;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        LOG.debug("Filtering {} to {} retaining {}", inputFile, outputFile, retainedAttributes);
+
+        final Manifest input;
+        try (InputStream is = Files.newInputStream(inputFile.toPath())) {
+            input = new Manifest(is);
+        } catch (IOException e) {
+            throw new MojoFailureException("Failed to read input " + inputFile, e);
+        }
+
+        // Keep only the entries we are instructed to keep
+        final Attributes inputAttrs = input.getMainAttributes();
+        LOG.debug("Input manifest has attributes {}", inputAttrs.keySet());
+
+        final Manifest output = new Manifest();
+        final Attributes outputAttrs = output.getMainAttributes();
+        // We need to always emit version
+        outputAttrs.putValue(Name.MANIFEST_VERSION.toString(), inputAttrs.getValue(Name.MANIFEST_VERSION));
+
+        for (String attr : retainedAttributes) {
+            final String value = inputAttrs.getValue(attr);
+            if (value != null) {
+                LOG.debug("Propagating attribute {} value {}", attr, value);
+                outputAttrs.putValue(attr, value);
+            } else {
+                LOG.debug("Skipping non-present attribute {}", attr);
+            }
+        }
+        LOG.debug("Output manifest has attributes {}", outputAttrs.keySet());
+
+        try (OutputStream os = Files.newOutputStream(outputFile.toPath())) {
+            output.write(os);
+        } catch (IOException e) {
+            throw new MojoFailureException("Failed to write output " + outputFile, e);
+        }
+    }
+}
