a9e960eed87e2cbad2511d89efea1e75f2b82ce9
Stephen Kitt <skitt@redhat.com>
2019-01-03 17:26:07 +0100

    Warn on duplicate feature dependencies
    When PopulateLocalRepoMojo encounters multiple versions for the
    dependencies pulled in through features, it now warns about them.
    
    JIRA: ODLPARENT-189
    Change-Id: I916e3dc47e109da9f727eff27b530796649a0218
    Signed-off-by: Stephen Kitt <skitt@redhat.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 8d2fcd2412da463c20e7565e3df6dd5294441231)

---
+++ karaf-plugin/src/main/java/org/opendaylight/odlparent/Gace.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2019 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent;
+
+import java.util.Objects;
+import org.eclipse.aether.artifact.Artifact;
+
+/**
+ * Group, artifact, classifier, extension.
+ */
+final class Gace {
+    private final String groupId;
+    private final String artifactId;
+    private final String classifier;
+    private final String extension;
+
+    Gace(Artifact artifact) {
+        groupId = artifact.getGroupId();
+        artifactId = artifact.getArtifactId();
+        classifier = artifact.getClassifier();
+        extension = artifact.getExtension();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Gace)) {
+            return false;
+        }
+        Gace gace = (Gace) obj;
+        return Objects.equals(groupId, gace.groupId)
+            && Objects.equals(artifactId, gace.artifactId)
+            && Objects.equals(classifier, gace.classifier)
+            && Objects.equals(extension, gace.extension);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(groupId, artifactId, classifier, extension);
+    }
+
+    @Override
+    public String toString() {
+        return groupId + ":" + artifactId + ":" + classifier + ":" + extension;
+    }
+}
