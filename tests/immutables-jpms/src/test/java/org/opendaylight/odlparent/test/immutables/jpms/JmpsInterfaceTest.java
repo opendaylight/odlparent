aecd81c7552fb173b5ccb002421820a37ab62f4d
Robert Varga <robert.varga@pantheon.tech>
2019-11-04 11:52:22 +0100

    Add immutables.value to annotationProcessorPaths
    Immutables do not work in our setup with JPMS modules, as they
    are not picked up from general dependency path. Also we do not need
    the full dependency in each module, just the annotations package.
    
    This is based on:
    https://github.com/immutables/immutables/issues/713#issuecomment-419750206
    https://github.com/immutables/immutables/issues/750
    https://github.com/immutables/immutables/issues/745#issuecomment-375952158
    
    JIRA: ODLPARENT-216
    Change-Id: Ib1b6f89b537fe27dbf68bec166b7ebdab042c483
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ tests/immutables-jpms/src/test/java/org/opendaylight/odlparent/test/immutables/jpms/JmpsInterfaceTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.test.immutables.jpms;
+
+import static org.hamcrest.Matchers.isA;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class JmpsInterfaceTest {
+    @Test
+    public void testBuilder() {
+        assertThat(ImmutableJpmsInterface.builder().foo(1).build(), isA(JpmsInterface.class));
+    }
+}
---
+++ tests/immutables-plain/src/test/java/org/opendaylight/odlparent/test/immutables/plain/PlainInterfaceTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.test.immutables.plain;
+
+import static org.hamcrest.Matchers.isA;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class PlainInterfaceTest {
+    @Test
+    public void testBuilder() {
+        assertThat(ImmutablePlainInterface.builder().bar(1).build(), isA(PlainInterface.class));
+    }
+}
