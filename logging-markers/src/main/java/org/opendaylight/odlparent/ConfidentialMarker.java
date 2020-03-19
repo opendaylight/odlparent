602376386e2ac73b3252ec8e437bf9ea3a9bf6e4
Tibor Král <tibor.kral@pantheon.tech>
2020-03-19 18:00:04 +0100

    Add logging markers to odlparent-artifacts which can be used to mark and filter-out sensitive data exposed in logs.
    Signed-off-by: Tibor Král <tibor.kral@pantheon.tech>
    Change-Id: I6c7071f548806759799df489d9e93fdf19ef4643

---
+++ logging-markers/src/main/java/org/opendaylight/odlparent/ConfidentialMarker.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent;
+
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+public class ConfidentialMarker {
+
+    private static String CONFIDENTIAL_TAG = "CONFIDENTIAL";
+
+    private final Marker marker;
+    private static ConfidentialMarker instance = null;
+
+    private ConfidentialMarker() {
+        this.marker = MarkerFactory.getMarker(CONFIDENTIAL_TAG);
+    }
+
+    public static ConfidentialMarker getInstance() {
+        if (instance == null) {
+            instance = new ConfidentialMarker();
+        }
+        return instance;
+    }
+
+    public Marker getMarker() {
+        return marker;
+    }
+}
