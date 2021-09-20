d1eba321d8d77e33510c976b00f2a9f61732cccc
Dominik Vrbovsky <dominik.vrbovsky@pantheon.tech>
2021-09-20 11:45:18 +0200

    WIP: Get rid of script-maven-plugin
    Create copy-files-plugin instead of using script-maven-plugin in odlparent/pom.xml
    
    // FIXME: fix a copy-files-plugin/pom.xml (parent)
    
    JIRA: ODLPARENT-220
    Change-Id: I5f1f5cee301767d7293be5e7816fac5eb450b831
    Signed-off-by: Dominik Vrbovsky <dominik.vrbovsky@pantheon.tech>

---
+++ copy-files-plugin/src/main/java/org/opendaylight/odlparent/copy/files/plugin/CopyFilesMojo.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.copy.files.plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "copy-files")
+public class CopyFilesMojo extends AbstractMojo {
+    @Parameter(required = true)
+    private File gitRepoRootDir;
+
+    @Parameter(required = true)
+    private File target;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        while (!new File(gitRepoRootDir, ".git").exists() && gitRepoRootDir.getParentFile() != null) {
+            gitRepoRootDir = gitRepoRootDir.getParentFile();
+        }
+        if (target.exists() || target.mkdirs()) {
+            copy(gitRepoRootDir, "README*", target);
+            copy(gitRepoRootDir, "CONTRIBUTING*", target);
+            copy(gitRepoRootDir, "INFO.yaml", target);
+        } else {
+            throw new MojoExecutionException("Failed to create the target file " + target);
+        }
+    }
+
+    private static void copy(File root, String glob, File target) throws MojoExecutionException {
+        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(root.toPath(), glob)) {
+            for (Path path : dirStream) {
+                Files.copy(path, new File(target, path.toFile().getName()).toPath(),
+                        StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+            }
+        } catch (IOException e) {
+            throw new MojoExecutionException("Failed to read a file", e);
+        }
+    }
+
+
+}
