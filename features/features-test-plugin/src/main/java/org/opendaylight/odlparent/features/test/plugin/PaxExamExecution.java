75c0e110763c6325414e0e5a025c12993c9953eb
Robert Varga <robert.varga@pantheon.tech>
2021-10-03 19:04:56 +0200

    WIP: Rework SingleFeatureTest
    SingleFeatureTest needs protection from concurrent exection by default.
    We just cannot assume the spawned container does not allocate global
    resources, like listening on all IP addresses' specific port.
    
    Features that are known to be safe can define sft.concurrent to enable
    concurrent exection, otherwise each executing Maven reactor will ensure
    testing executes in at most a single reactor.
    
    // FIXME: finish invoking karaf
    // FIXME: test parallel execution
    
    JIRA: ODLPARENT-262
    Change-Id: Ib26b4ac55f4bc689a9f459f6e296df65a75ef645
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ features/features-test-plugin/src/main/java/org/opendaylight/odlparent/features/test/plugin/PaxExamExecution.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2023 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.features.test.plugin;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ServiceLoader;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.ops4j.pax.exam.ExamSystem;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.TestContainer;
+import org.ops4j.pax.exam.TestContainerFactory;
+import org.ops4j.pax.exam.options.extra.VMOption;
+import org.ops4j.pax.exam.spi.DefaultExamSystem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class PaxExamExecution {
+    private static final Logger LOG = LoggerFactory.getLogger(PaxExamExecution.class);
+
+    private final List<TestContainer> containers;
+    private final String localRepository;
+    private final String featureDir;
+
+    PaxExamExecution(final String featureDir, final String localRepository, final String maxHeap,
+            final String heapDumpPath) throws MojoExecutionException {
+        this.featureDir = requireNonNull(featureDir);
+        this.localRepository = requireNonNull(localRepository);
+
+        final var containerFactory = ServiceLoader.load(TestContainerFactory.class).findFirst()
+            .orElseThrow(() -> new MojoExecutionException("Cannot find a TestContainerFactory"));
+
+        final var options = new ArrayList<Option>();
+
+        options.add(new VMOption("-Xmx" + maxHeap));
+        options.add(new VMOption("-XX:+HeapDumpOnOutOfMemoryError"));
+        options.add(new VMOption("-XX:HeapDumpPath=" + heapDumpPath));
+
+        // FIXME: fill in other options
+
+        final ExamSystem system;
+        try {
+            system = DefaultExamSystem.create(options.toArray(new Option[0]));
+        } catch (IOException e) {
+            throw new MojoExecutionException("Cannot create pax-exam system", e);
+        }
+
+        containers = List.of(containerFactory.create(system));
+    }
+
+    void execute() throws MojoExecutionException {
+        // Use the same repository for Pax Exam as is used for Maven
+        System.setProperty("org.ops4j.pax.url.mvn.localRepository", localRepository);
+        LOG.debug("Running features in {} with local repository at {} with {}", featureDir, localRepository,
+            containers);
+
+        // FIXME: implement this
+        throw new MojoExecutionException("Not implemented yet");
+    }
+}
