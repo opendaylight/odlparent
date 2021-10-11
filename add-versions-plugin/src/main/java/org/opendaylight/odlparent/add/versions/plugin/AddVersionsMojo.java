6575b665620bcbbcf3ea3d679e198e5cd42db18b
Dominik Vrbovsky <dominik.vrbovsky@pantheon.tech>
2021-10-11 09:28:47 +0200

    Add a parent for static features
    Create a maven plugin, add-versions-plugin, which replaces mustache-enclosed
    tags in feature.xml with versions from pom.xml
    
    Add the parent POM for static features, which contains the add-versions-plugin
    
    JIRA: ODLPARENT-235
    Change-Id: I2a8e1b4114e9bb2954ede8511bd991b0fca781cd
    Signed-off-by: Dominik Vrbovsky <dominik.vrbovsky@pantheon.tech>

---
+++ add-versions-plugin/src/main/java/org/opendaylight/odlparent/add/versions/plugin/AddVersionsMojo.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.add.versions.plugin;
+
+import java.io.File;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import net.sf.saxon.TransformerFactoryImpl;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "add-versions", threadSafe = true)
+public class AddVersionsMojo extends AbstractMojo {
+
+    private static final String FEATURE_FILE_PATH = "src/main/feature/feature.xml";
+
+    @Parameter(required = true, defaultValue = "${project}", readonly = true)
+    private MavenProject mavenProject;
+
+    @Parameter(required = true, defaultValue = "${project.basedir}", readonly = true)
+    private File projectBasedir;
+
+    @Parameter(required = true, defaultValue = "${project.build.directory}", readonly = true)
+    private File projectTargetDir;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        final Source srcFeature = new StreamSource(new File(projectBasedir, FEATURE_FILE_PATH));
+        final Source srcXslt = new StreamSource(AddVersionsMojo.class.getResourceAsStream("/feature.xsl"));
+
+        final File dstDir = new File(projectTargetDir, "generated-resources/feature");
+        if (!dstDir.mkdirs()) {
+            throw new MojoExecutionException("Failed to create directory " + dstDir);
+        }
+        final Result resultFeature = new StreamResult(new File(dstDir, "feature.xml"));
+
+        try {
+            final Transformer transformer = new TransformerFactoryImpl().newTransformer(srcXslt);
+            transformer.setParameter("mavenProject", mavenProject);
+            transformer.transform(srcFeature, resultFeature);
+        } catch (TransformerException e) {
+            throw new MojoExecutionException("Failed to perform transformation", e);
+        }
+    }
+}
