38d7a9b121f46cadcff6f0ac8839549a45e5b3f6
Robert Varga <robert.varga@pantheon.tech>
2024-04-23 00:47:49 +0200

    WIP: Add bundles-diag
    This is a distillation of what we can report from Karaf's BundleService,
    so that it can be used by multiple users.
    
    // FIXME: clean up the logging bits
    
    JIRA: ODLPARENT-312
    Change-Id: I2c151926bbe386a81566f42b206a60100ecf4178
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- bundles-test-lib/src/main/java/org/opendaylight/odlparent/bundlestest/lib/BundleDiagInfosImpl.java
+++ bundles-test-lib/src/main/java/org/opendaylight/odlparent/bundlestest/lib/BundleDiagInfosImpl.java
@@ -7,20 +7,16 @@
  */
 package org.opendaylight.odlparent.bundlestest.lib;
 
-import static org.apache.karaf.bundle.core.BundleState.Active;
-import static org.apache.karaf.bundle.core.BundleState.Installed;
-import static org.apache.karaf.bundle.core.BundleState.Waiting;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.apache.karaf.bundle.core.BundleInfo;
 import org.apache.karaf.bundle.core.BundleService;
-import org.apache.karaf.bundle.core.BundleState;
-import org.osgi.framework.Bundle;
+import org.opendaylight.odlparent.bundles.diag.ContainerState;
+import org.opendaylight.odlparent.bundles.diag.Diag;
+import org.opendaylight.odlparent.bundles.diag.spi.DefaultDiagProvider;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -33,16 +29,16 @@ final class BundleDiagInfosImpl implements BundleDiagInfos {
     @java.io.Serial
     private static final long serialVersionUID = 1L;
 
-    private static final Map<String, BundleState> WHITELISTED_BUNDLES = Map.of(
-        "slf4j.log4j12", Installed,
+    private static final Map<String, ContainerState> WHITELISTED_BUNDLES = Map.of(
+        "slf4j.log4j12", ContainerState.INSTALLED,
         // ODLPARENT-144
-        "org.apache.karaf.scr.management", Waiting);
+        "org.apache.karaf.scr.management", ContainerState.WAITING);
 
     private final List<String> okBundleStateInfoTexts;
     private final List<String> nokBundleStateInfoTexts;
     private final List<String> whitelistedBundleStateInfoTexts;
-    private final Map<BundleState, Integer> bundleStatesCounters;
-    private final Map<BundleSymbolicNameWithVersion, BundleState> bundlesStateMap;
+    private final Map<ContainerState, Integer> bundleStatesCounters;
+    private final Map<BundleSymbolicNameWithVersion, ContainerState> bundlesStateMap;
 
     /**
      * Create an instance. The collections provided as arguments will be kept as-is; it’s up to the caller
@@ -55,8 +51,8 @@ final class BundleDiagInfosImpl implements BundleDiagInfos {
      * @param bundlesStateMap bundle state map (state of each bundle).
      */
     private BundleDiagInfosImpl(List<String> okBundleStateInfoTexts, List<String> nokBundleStateInfoTexts,
-            List<String> whitelistedBundleStateInfoTexts, Map<BundleState, Integer> bundleStatesCounters,
-            Map<BundleSymbolicNameWithVersion, BundleState> bundlesStateMap) {
+            List<String> whitelistedBundleStateInfoTexts, Map<ContainerState, Integer> bundleStatesCounters,
+            Map<BundleSymbolicNameWithVersion, ContainerState> bundlesStateMap) {
         this.okBundleStateInfoTexts = okBundleStateInfoTexts;
         this.nokBundleStateInfoTexts = nokBundleStateInfoTexts;
         this.whitelistedBundleStateInfoTexts = whitelistedBundleStateInfoTexts;
@@ -65,30 +61,34 @@ final class BundleDiagInfosImpl implements BundleDiagInfos {
     }
 
     public static BundleDiagInfos forContext(BundleContext bundleContext, BundleService bundleService) {
+        return ofDiag(new DefaultDiagProvider(bundleService, bundleContext).currentDiag());
+    }
+
+    public static BundleDiagInfos ofDiag(Diag diag) {
         List<String> okBundleStateInfoTexts = new ArrayList<>();
         List<String> nokBundleStateInfoTexts = new ArrayList<>();
         List<String> whitelistedBundleStateInfoTexts = new ArrayList<>();
-        Map<BundleSymbolicNameWithVersion, BundleState> bundlesStateMap = new HashMap<>();
-        Map<BundleState, Integer> bundleStatesCounters = new EnumMap<>(BundleState.class);
-        for (BundleState bundleState : BundleState.values()) {
+        Map<BundleSymbolicNameWithVersion, ContainerState> bundlesStateMap = new HashMap<>();
+        Map<ContainerState, Integer> bundleStatesCounters = new EnumMap<>(ContainerState.class);
+        for (var bundleState : ContainerState.values()) {
             bundleStatesCounters.put(bundleState, 0);
         }
 
-        for (Bundle bundle : bundleContext.getBundles()) {
-            String bundleSymbolicName = bundle.getSymbolicName();
-            BundleSymbolicNameWithVersion bundleSymbolicNameWithVersion
-                = new BundleSymbolicNameWithVersion(bundleSymbolicName, bundle.getVersion().toString());
+        for (var bundle : diag.bundles()) {
+            final var bundleSymbolicName = bundle.symbolicName();
+            final var bundleSymbolicNameWithVersion = new BundleSymbolicNameWithVersion(bundleSymbolicName,
+                bundle.version());
 
-            BundleInfo karafBundleInfo = bundleService.getInfo(bundle);
-            String diagText = bundleService.getDiag(bundle);
-            BundleState karafBundleState = karafBundleInfo.getState();
-            bundlesStateMap.put(bundleSymbolicNameWithVersion, karafBundleState);
+            final var serviceState = bundle.serviceState();
+            final var diagText = serviceState.diag();
+            final var karafBundleState = serviceState.containerState();
+            bundlesStateMap.put(bundleSymbolicNameWithVersion, serviceState.containerState());
 
-            String bundleStateDiagText = "OSGi state = " + bundleStateToText(bundle.getState())
-                + ", Karaf bundleState = " + karafBundleState
+            String bundleStateDiagText = "OSGi state = " + bundle.frameworkState().symbolicName()
+                + ", Karaf bundleState = " + karafBundleState.name()
                 + (diagText.isEmpty() ? "" : ", due to: " + diagText);
 
-            if (bundleSymbolicName != null && WHITELISTED_BUNDLES.get(bundleSymbolicName) == karafBundleState) {
+            if (bundleSymbolicName != null && karafBundleState.equals(WHITELISTED_BUNDLES.get(bundleSymbolicName))) {
                 whitelistedBundleStateInfoTexts.add(
                     "WHITELISTED " + bundleSymbolicNameWithVersion + ": " + bundleStateDiagText);
                 continue;
@@ -101,7 +101,7 @@ final class BundleDiagInfosImpl implements BundleDiagInfos {
             // but we intentionally, got a little further than Karaf's "diag" command,
             // and instead of only checking some states, we check what's really Active,
             // but accept that some remain just Resolved:
-            if (karafBundleState != Active && karafBundleState != BundleState.Resolved) {
+            if (karafBundleState != ContainerState.ACTIVE && karafBundleState != ContainerState.RESOLVED) {
                 String msg = "NOK " + bundleSymbolicNameWithVersion + ": " + bundleStateDiagText;
                 nokBundleStateInfoTexts.add(msg);
             } else {
@@ -116,33 +116,21 @@ final class BundleDiagInfosImpl implements BundleDiagInfos {
                 Collections.unmodifiableMap(bundleStatesCounters), Collections.unmodifiableMap(bundlesStateMap));
     }
 
-    private static String bundleStateToText(int state) {
-        return switch (state) {
-            case Bundle.INSTALLED -> "Installed";
-            case Bundle.RESOLVED -> "Resolved";
-            case Bundle.STARTING -> "Starting";
-            case Bundle.ACTIVE -> "Active";
-            case Bundle.STOPPING -> "Stopping";
-            case Bundle.UNINSTALLED -> "Uninstalled";
-            default -> state + "???";
-        };
-    }
-
     @Override
     public SystemState getSystemState() {
-        if (bundleStatesCounters.get(BundleState.Failure) > 0) {
+        if (bundleStatesCounters.get(ContainerState.FAILURE) > 0) {
             return SystemState.Failure;
-        } else if (bundleStatesCounters.get(BundleState.Stopping) > 0) {
+        } else if (bundleStatesCounters.get(ContainerState.STOPPING) > 0) {
             return SystemState.Stopping;
-        } else if (bundleStatesCounters.get(BundleState.Installed) == 0
+        } else if (bundleStatesCounters.get(ContainerState.INSTALLED) == 0
                 // No, just Resolved is OK, so do not: && bundleStatesCounters.get(BundleState.Resolved) == 0
-                && bundleStatesCounters.get(BundleState.Unknown) == 0
-                && bundleStatesCounters.get(BundleState.GracePeriod) == 0
-                && bundleStatesCounters.get(BundleState.Waiting) == 0
-                && bundleStatesCounters.get(BundleState.Starting) == 0
+                && bundleStatesCounters.get(ContainerState.UNKNOWN) == 0
+                && bundleStatesCounters.get(ContainerState.GRACE_PERIOD) == 0
+                && bundleStatesCounters.get(ContainerState.WAITING) == 0
+                && bundleStatesCounters.get(ContainerState.STARTING) == 0
                 // BundleState.Active *should* be ~= total # of bundles (minus Resolved, and whitelisted installed)
-                && bundleStatesCounters.get(BundleState.Stopping) == 0
-                && bundleStatesCounters.get(BundleState.Failure) == 0) {
+                && bundleStatesCounters.get(ContainerState.STOPPING) == 0
+                && bundleStatesCounters.get(ContainerState.FAILURE) == 0) {
             return SystemState.Active;
         } else {
             return SystemState.Booting;
@@ -168,7 +156,7 @@ final class BundleDiagInfosImpl implements BundleDiagInfos {
     }
 
     @Override
-    public Map<BundleSymbolicNameWithVersion, BundleState> getBundlesStateMap() {
+    public Map<BundleSymbolicNameWithVersion, ContainerState> getBundlesStateMap() {
         return bundlesStateMap;
     }
 
