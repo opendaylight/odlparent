4b6ad7409a6f3c2b20aea76125902caa4658cc66
Robert Varga <robert.varga@pantheon.tech>
2024-04-22 21:17:37 +0200

    Refactor TestProbe check
    Isolate the managed state into an explicit inner class, making it
    clearer as to what is going on.
    
    Also improve transitions and logging, so that we have better information
    about how the probe is doing.
    
    Change-Id: I15defea8fc085d4dd89e964e58c5dbc56d07564c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- features-test-plugin/src/main/java/org/opendaylight/odlparent/features/test/plugin/TestProbe.java
+++ features-test-plugin/src/main/java/org/opendaylight/odlparent/features/test/plugin/TestProbe.java
@@ -17,7 +17,6 @@ import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -31,6 +30,7 @@ import org.apache.karaf.features.FeaturesService;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
 import org.osgi.framework.BundleListener;
 import org.osgi.framework.InvalidSyntaxException;
 import org.slf4j.Logger;
@@ -79,6 +79,152 @@ public final class TestProbe {
         }
     }
 
+    private final class BundleCheck implements BundleListener, AutoCloseable {
+        private final AtomicReference<CompletableFuture<CheckResult>> checkFutureRef = new AtomicReference<>();
+        private final Map<Long, CheckResult> bundleCheckResults;
+
+        // We are being invoked outside of framework tread, hence things may be happening asynchronously. Let's tread
+        // very carefully here
+        BundleCheck() {
+            // Populate initial bundle states...
+            bundleCheckResults = Arrays.stream(bundleContext.getBundles())
+                .collect(Collectors.toMap(Bundle::getBundleId, bundle -> {
+                    final var info = bundleService.getInfo(bundle);
+                    final var result = CheckResult.of(info.getSymbolicName(), info.getState());
+                    LOG.info("Initial bundle state: [{}]{} -> {} ({})", bundle.getBundleId(), info.getSymbolicName(),
+                        info.getState(), result);
+                    return result;
+                }));
+
+            // ... register as listener, enabling bundleChanged() delivery, but be careful about completing the future
+            synchronized (this) {
+                // this may trigger bundleChanged() events being proceessed due to reentrant locking
+                bundleContext.addBundleListener(this);
+
+                // some time may have passed between initial state and listener seeing state, hence we need to re-run
+                // updates by observing bundles again.
+                for (var bundle : bundleContext.getBundles()) {
+                    updateBundle(bundle);
+                }
+
+                // finally enable completion and perform first check
+                final var checkFuture = new CompletableFuture<CheckResult>();
+                checkFutureRef.set(checkFuture);
+                updateCheckResults(checkFuture);
+            }
+        }
+
+        void getResult(final int timeoutSeconds) {
+            final CheckResult result;
+            try {
+                result = checkFutureRef.get().get(timeoutSeconds, TimeUnit.SECONDS);
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                logNokBundleDetails();
+                throw new IllegalStateException("Bundles states check was not completed", e);
+            }
+
+            LOG.info("Bundle state check completed with result {}", result);
+            if (result != CheckResult.SUCCESS) {
+                logNokBundleDetails();
+                throw new IllegalStateException("Bundle states check failed");
+            }
+        }
+
+        @Override
+        public synchronized void close() {
+            bundleContext.removeBundleListener(this);
+        }
+
+        @Override
+        public synchronized void bundleChanged(final BundleEvent event) {
+            LOG.info("Received {}", event);
+            final var bundle = event.getBundle();
+            if (bundle != null) {
+                updateBundle(bundle);
+                final var checkFuture = checkFutureRef.get();
+                if (checkFuture != null && !checkFuture.isDone()) {
+                    // update results if already initialized and check has not completed yet
+                    updateCheckResults(checkFuture);
+                }
+            }
+        }
+
+        private void updateBundle(final Bundle bundle) {
+            final var info = bundleService.getInfo(bundle);
+            final var next = CheckResult.of(info.getSymbolicName(), info.getState());
+            final Long bundleId = bundle.getBundleId();
+            final var prev = bundleCheckResults.put(bundleId, next);
+            LOG.info("Bundle state updated: [{}]{} -> {} ({} -> {})", bundleId, info.getSymbolicName(), info.getState(),
+                prev, next);
+        }
+
+        private void updateCheckResults(final CompletableFuture<CheckResult> checkFuture) {
+            final var results = bundleCheckResults.entrySet().stream()
+                .collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.counting()));
+            LOG.info("Bundle states check results: total={}, byResult={}", bundleCheckResults.size(), results);
+
+            final var result = computeResult(results);
+            if (result != null) {
+                checkFuture.complete(result);
+            }
+        }
+
+        private static CheckResult computeResult(final Map<CheckResult, Long> results) {
+            final var failures = results.get(CheckResult.FAILURE);
+            if (failures != null && failures > 0) {
+                LOG.info("Observed {} failured bundles, completing", failures);
+                return CheckResult.FAILURE;
+            }
+            final var stopping = results.get(CheckResult.STOPPING);
+            if (stopping != null && stopping > 0) {
+                LOG.info("Observed {} stopping bundles, completing", failures);
+                return CheckResult.STOPPING;
+            }
+            final var inProgress = results.get(CheckResult.IN_PROGRESS);
+            if (inProgress != null && inProgress > 0) {
+                LOG.info("Observed {} bundles in progress, waiting for more updates", inProgress);
+                return null;
+            }
+
+            LOG.info("Observed {} successful bundles, completing", results.getOrDefault(CheckResult.SUCCESS, 0L));
+            return CheckResult.SUCCESS;
+        }
+
+        private synchronized void logNokBundleDetails() {
+            final var nokBundles = bundleCheckResults.entrySet().stream()
+                .filter(entry -> CheckResult.SUCCESS != entry.getValue())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+            // log NOK Bundles
+            for (var bundle : bundleContext.getBundles()) {
+                if (nokBundles.contains(bundle.getBundleId())) {
+                    final var info = bundleService.getInfo(bundle);
+                    final var diag = bundleService.getDiag(bundle);
+                    LOG.warn("NOK Bundle {} -> State: {}{}", info.getSymbolicName(), info.getState(),
+                        diag.isEmpty() ? "" : ", due to: " + diag);
+                }
+            }
+            // log services of NOK bundles
+            try {
+                for (var serviceRef : bundleContext.getAllServiceReferences(null, null)) {
+                    final var bundle = serviceRef.getBundle();
+                    if (bundle != null && nokBundles.contains(bundle.getBundleId())) {
+                        final var usingBundles = serviceRef.getUsingBundles();
+                        final var usingSymbolic = usingBundles == null ? List.of()
+                            : Arrays.stream(usingBundles).map(Bundle::getSymbolicName).toList();
+                        final var propKeys = serviceRef.getPropertyKeys();
+                        final var serviceProps = Arrays.stream(propKeys)
+                            .collect(Collectors.toMap(Function.identity(), serviceRef::getProperty));
+                        LOG.warn("NOK Service {} -> of bundle: {}, using: {}, props: {}",
+                            serviceRef.getClass().getName(), bundle.getSymbolicName(), usingSymbolic, serviceProps);
+                    }
+                }
+            } catch (InvalidSyntaxException e) {
+                LOG.warn("Error retrieving services", e);
+            }
+        }
+    }
+
     static final String FEATURE_FILE_URI_PROP = "feature.test.file.uri";
     static final String BUNDLE_CHECK_SKIP = "feature.test.bundle.check.skip";
     static final String BUNDLE_CHECK_TIMEOUT_SECONDS = "feature.test.bundle.check.timeout.seconds";
@@ -88,17 +234,13 @@ public final class TestProbe {
     private static final Logger LOG = LoggerFactory.getLogger(TestProbe.class);
     private static final Map<String, BundleState> ELIGIBLE_STATES = Map.of(
         "slf4j.log4j12", Installed,
+        // see https://jira.opendaylight.org/browse/ODLPARENT-144
         "org.apache.karaf.scr.management", Waiting);
-
-    private final Map<Long, CheckResult> bundleCheckResults = new ConcurrentHashMap<>();
-    private final AtomicReference<CompletableFuture<CheckResult>> checkFutureRef = new AtomicReference<>();
 
     @Inject
     private BundleContext bundleContext;
-
     @Inject
     private FeaturesService featuresService;
-
     @Inject
     private BundleService bundleService;
 
@@ -156,101 +298,15 @@ public final class TestProbe {
         }
     }
 
-    private void checkBundleStates() throws InterruptedException, ExecutionException {
+    private void checkBundleStates() {
         if ("true".equals(System.getProperty(BUNDLE_CHECK_SKIP))) {
             return;
         }
         final int timeout = Integer.parseInt(System.getProperty(BUNDLE_CHECK_TIMEOUT_SECONDS, "600"));
         LOG.info("Checking bundle states. Timeout is {} seconds.", timeout);
 
-        // start event based states collection
-        final BundleListener bundleListener = event -> {
-            captureBundleState(event.getBundle());
-            updateCheckResults();
-        };
-        bundleContext.addBundleListener(bundleListener);
-        // init all bundles state data
-        Arrays.stream(bundleContext.getBundles()).forEach(this::captureBundleState);
-        // enable stats analysis
-        final var checkFuture = new CompletableFuture<CheckResult>();
-        checkFutureRef.set(checkFuture);
-        // perform stats analysis
-        updateCheckResults();
-
-        final CheckResult result;
-        try {
-            result = checkFuture.get(timeout, TimeUnit.SECONDS);
-        } catch (TimeoutException e) {
-            logNokBundleDetails();
-            throw new IllegalStateException("Bundles states check was not completed in " + timeout + "seconds", e);
-        } finally {
-            bundleContext.removeBundleListener(bundleListener);
-        }
-        LOG.info("Bundle state check completed with result {}", result);
-        if (result != CheckResult.SUCCESS) {
-            logNokBundleDetails();
-            throw new IllegalStateException("Bundle states check failed");
-        }
-    }
-
-    private void captureBundleState(final Bundle bundle) {
-        if (bundle != null) {
-            final var info = bundleService.getInfo(bundle);
-            final var checkResult = CheckResult.of(info.getSymbolicName(), info.getState());
-            LOG.info("Bundle state updated: {} -> {} ({})", info.getSymbolicName(), info.getState(), checkResult);
-            bundleCheckResults.put(bundle.getBundleId(), checkResult);
-        }
-    }
-
-    private void updateCheckResults() {
-        final var checkFuture = checkFutureRef.get();
-        if (checkFuture == null || checkFuture.isDone()) {
-            // don't check stats if results are not expected or already delivered
-            return;
-        }
-        final var resultStats = bundleCheckResults.entrySet().stream()
-            .collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.counting()));
-        LOG.info("Bundle states check results: total={}, byResult={}", bundleCheckResults.size(), resultStats);
-
-        if (resultStats.getOrDefault(CheckResult.FAILURE, 0L) > 0) {
-            checkFuture.complete(CheckResult.FAILURE);
-        } else if (resultStats.getOrDefault(CheckResult.STOPPING, 0L) > 0) {
-            checkFuture.complete(CheckResult.STOPPING);
-        } else if (resultStats.getOrDefault(CheckResult.IN_PROGRESS, 0L) == 0) {
-            checkFuture.complete(CheckResult.SUCCESS);
-        }
-    }
-
-    private void logNokBundleDetails() {
-        final var nokBundles = bundleCheckResults.entrySet().stream()
-            .filter(entry -> CheckResult.SUCCESS != entry.getValue())
-            .map(Map.Entry::getKey).collect(Collectors.toSet());
-        // log NOK Bundles
-        for (var bundle : bundleContext.getBundles()) {
-            if (nokBundles.contains(bundle.getBundleId())) {
-                final var info = bundleService.getInfo(bundle);
-                final var diag = bundleService.getDiag(bundle);
-                LOG.warn("NOK Bundle {} -> State: {}{}", info.getSymbolicName(), info.getState(),
-                    diag.isEmpty() ? "" : ", due to: " + diag);
-            }
-        }
-        // log services of NOK bundles
-        try {
-            for (var serviceRef : bundleContext.getAllServiceReferences(null, null)) {
-                final var bundle = serviceRef.getBundle();
-                if (bundle != null && nokBundles.contains(bundle.getBundleId())) {
-                    final var usingBundles = serviceRef.getUsingBundles();
-                    final var usingSymbolic = usingBundles == null ? List.of()
-                        : Arrays.stream(usingBundles).map(Bundle::getSymbolicName).toList();
-                    final var propKeys = serviceRef.getPropertyKeys();
-                    final var serviceProps = Arrays.stream(propKeys)
-                        .collect(Collectors.toMap(Function.identity(), serviceRef::getProperty));
-                    LOG.warn("NOK Service {} -> of bundle: {}, using: {}, props: {}",
-                        serviceRef.getClass().getName(), bundle.getSymbolicName(), usingSymbolic, serviceProps);
-                }
-            }
-        } catch (InvalidSyntaxException e) {
-            LOG.warn("Error retrieving services", e);
+        try (var check = new BundleCheck()) {
+            check.getResult(timeout);
         }
     }
 }
