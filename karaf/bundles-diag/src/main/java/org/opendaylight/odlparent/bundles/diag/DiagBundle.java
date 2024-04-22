7a326a0a4a5587a3f52419a01fea63e48aece177
Robert Varga <robert.varga@pantheon.tech>
2024-04-23 00:47:49 +0200

    Add bundles-diag
    This is a distillation of what we can report from Karaf's BundleService,
    so that it can be used by multiple users.
    
    Utilized in feature-test-plugin to log bundle states for troubleshooting
    
    // FIXME: TestProbe.aggregatedCheckResults() and
    //        BundleDiagInfosImpl.getSystemState() should interface to
    //        shared code
    // FIXME: clean up diag status printout, so that we print it out only
    //        when not active
    
    JIRA: ODLPARENT-312
    Change-Id: I2c151926bbe386a81566f42b206a60100ecf4178
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ karaf/bundles-diag/src/main/java/org/opendaylight/odlparent/bundles/diag/DiagBundle.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.bundles.diag;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.Serializable;
+import org.eclipse.jdt.annotation.Nullable;
+import org.osgi.framework.Bundle;
+
+/**
+ * A {@link Bundle} that has been diagnosed.
+ *
+ * @param bundleId {@link Bundle#getBundleId()}
+ * @param name bundle name
+ * @param symbolicName bundle symbolic name
+ * @param version bundle version
+ * @param frameworkState current {@link FrameworkState}
+ * @param serviceState current {@link ServiceState}
+ */
+public record DiagBundle(
+        long bundleId,
+        String name,
+        @Nullable String symbolicName,
+        String version,
+        FrameworkState frameworkState,
+        ServiceState serviceState) implements Serializable {
+    // FIXME: remove once we have spotbugs-4.8.5+
+    @java.io.Serial
+    private static final long serialVersionUID = 0;
+
+    public DiagBundle {
+        // FIXME: handle pax-exam's test probe bundle with null name and version
+        // requireNonNull(name);
+        // requireNonNull(version);
+        requireNonNull(frameworkState);
+        requireNonNull(serviceState);
+    }
+}
