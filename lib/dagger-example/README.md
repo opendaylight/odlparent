dad4441374645ee1b3d08df683b0aecc3cc3f044
Robert Varga <robert.varga@pantheon.tech>
2025-11-05 00:47:55 +0100

    Add lib/dagger(-example)
    This adds a dagger-specific module to aid with resource cleanup. We also
    include an example of a full OSGi/Dagger bundle, along with a test
    feature.
    
    JIRA: ODLPARENT-282
    Change-Id: Ic426be53962f22e36af78cfdbe338dd042d61c2d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ lib/dagger-example/README.md
@@ -0,0 +1,29 @@
+## Example OpenDaylight Dagger-enabled artifact
+
+This artifact shows how Dagger is an alternative to OSGi Service Component Runtime, packaged as a full
+JPMS module.
+
+There are three package involved here:
+
+- [dagger.example.api](src/main/java/org/opendaylight/dagger/example/api/), which it the API being implemented.
+  Normally we keep these in separate artifacts to explicitly separate API from implementation. It is exported
+  from the module and made available in OSGi as well.
+
+- [dagger.example](src/main/java/org/opendaylight/dagger/example/), which hosts the implementation. It is not
+  exported from the module, nor is it available in OSGi.
+
+- [dagger.example.dagger](src/main/java/org/opendaylight/dagger/example/dagger/), which hosts Dagger modules
+  and components, along with whatever code Dagger generates for these. This package is not exposed in OSGi.
+
+In order to achieve this, details are quite important:
+
+- [pom.xml](pom.xml) based on `bnd-parent`. This provides the substrate for processing OSGi annotations and
+  providing correct `MANIFEST.MF` entries.
+
+- [module-info.java](src/main/java/module-info.java) needs to be present and do the right thing. Most notably
+  we should not `requires transitive` any Dagger-related modules if we are exporting any other package aside
+  from the `.dagger` package. We do that here, as we are also exporting `example.api`, hence we use
+  `requires static`.
+
+- [bnd.bnd](bnd.bnd) file, which needs to update the default policy, so that `dagger`, `jakarta.inject` etc.
+  packages are not required in OSGi.
---
+++ lib/dagger/src/main/java/org/opendaylight/odlparent/dagger/ResourceSupport.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.dagger;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.UUID;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A registry for {@link AutoCloseable} resources to close when a component is shut down. This class should be used via
+ * {@link ResourceSupportModule}.
+ */
+@NonNullByDefault
+public final class ResourceSupport {
+    private sealed interface State {
+        // Marker interface
+    }
+
+    private static final class Closed implements State {
+        static final Closed INSTANCE = new Closed();
+
+        private Closed() {
+            // Hidden on purpose
+        }
+    }
+
+    private record Open(ArrayList<AutoCloseable> resources) implements State {
+        Open {
+            requireNonNull(resources);
+        }
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(ResourceSupport.class);
+
+    private final UUID uuid = UUID.randomUUID();
+
+    private State state = new Open(new ArrayList<>());
+
+    ResourceSupport() {
+        // Hidden on purpose
+    }
+
+    /**
+     * Register an {@link AutoCloseable} resource with this registry.
+     *
+     * @param <T> resource type
+     * @param resource the resource
+     * @return the resource
+     * @throws IllegalStateException if this instance has been closed
+     */
+    public <T extends AutoCloseable> T register(final T resource) {
+        final var checked = requireNonNull(resource);
+
+        synchronized (this) {
+            switch (state) {
+                case Closed close -> throw new IllegalStateException("Cannot register " + checked + " while closed");
+                case Open(var resources) -> resources.add(checked);
+            }
+        }
+
+        return checked;
+    }
+
+    @Override
+    public String toString() {
+        final State local;
+        synchronized (this) {
+            local = state;
+        }
+        return "ResourceSupport [uuid=" + uuid + ", state=" + local.getClass().getSimpleName() + "]";
+    }
+
+    // Hidden on purpose
+    void close() {
+        final Iterator<AutoCloseable> it;
+
+        synchronized (this) {
+            switch (state) {
+                case Closed closed -> {
+                    LOG.debug("Ignoring duplicate close() on {}", uuid);
+                    return;
+                }
+                case Open(var resources) -> {
+                    LOG.debug("Closing instance {} with {} resources", uuid, resources.size());
+                    state = Closed.INSTANCE;
+                    it = resources.reversed().iterator();
+                }
+            }
+        }
+
+        it.forEachRemaining(ResourceSupport::closeDefensively);
+
+        LOG.debug("Instance {} closed", uuid);
+    }
+
+    @SuppressWarnings("checkstyle:illegalCatch")
+    private static void closeDefensively(final AutoCloseable resource) {
+        try {
+            resource.close();
+        } catch (Exception e) {
+            LOG.warn("Failed to close resource {}", resource, e);
+        }
+    }
+}
