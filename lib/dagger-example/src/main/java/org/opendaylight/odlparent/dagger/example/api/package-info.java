bd1ccc8a88de28df4085038a8c8b3d2474ec4363
Robert Varga <robert.varga@pantheon.tech>
2025-11-05 00:47:55 +0100

    Add lib/dagger(-example)
    This adds a dagger-specific module to aid with resource cleanup. We also
    include an example of a full OSGi/Dagger bundle, along with a test
    feature.
    
    JIRA: ODLPARENT-282
    Change-Id: Ic426be53962f22e36af78cfdbe338dd042d61c2d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ lib/dagger-example/src/main/java/org/opendaylight/odlparent/dagger/example/api/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * The API, which defines {@link ExampleService}. This package lives in this artifact only to keep the example compact,
+ * normally it would be in a completely separated artifact, such as {@code dagger-example-api}.
+ */
+@org.osgi.annotation.bundle.Export
+package org.opendaylight.odlparent.dagger.example.api;
