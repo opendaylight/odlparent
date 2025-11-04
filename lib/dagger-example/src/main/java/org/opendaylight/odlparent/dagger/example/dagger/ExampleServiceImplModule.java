5557a16367336d89f9dc814c61a58e0396302df4
Robert Varga <robert.varga@pantheon.tech>
2025-11-05 00:47:55 +0100

    Add lib/dagger(-example)
    This adds a dagger-specific module to aid with resource cleanup. We also
    include an example of a full OSGi/Dagger bundle, along with a test
    feature.
    
    JIRA: ODLPARENT-282
    Change-Id: Ic426be53962f22e36af78cfdbe338dd042d61c2d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ lib/dagger-example/src/main/java/org/opendaylight/odlparent/dagger/example/dagger/ExampleServiceImplModule.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.dagger.example.dagger;
+
+import dagger.Module;
+import dagger.Provides;
+import jakarta.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.odlparent.dagger.ResourceSupport;
+import org.opendaylight.odlparent.dagger.example.ExampleServiceImpl;
+import org.opendaylight.odlparent.dagger.example.api.ExampleService;
+
+/**
+ * A Dagger module binding {@link ExampleServiceImpl}.
+ */
+@Module
+@NonNullByDefault
+public interface ExampleServiceImplModule {
+    /**
+     * Provide an {@link ExampleService}.
+     *
+     * @param resourceSupport the {@link ResourceSupport} to register with
+     * @return an {@link ExampleService}
+     */
+    @Provides
+    @Singleton
+    static ExampleService provideExampleService(final ResourceSupport resourceSupport) {
+        // this is the magic incantation
+        return resourceSupport.register(new ExampleServiceImpl());
+    }
+}
---
+++ lib/dagger/README.md
@@ -0,0 +1,7 @@
+## OpenDaylight [Dagger](dagger.dev) support
+
+This artifact exposes common lifecycle hooks for use with Dagger dependendecy injection framework. This
+includes `org.opendaylight.odlparent.dagger.AutoCloseableComponent` and its related `ResourceSupport`
+and `ResourceSupportModule`.
+
+See the [example project](../dagger-example) for detailed instructions.
