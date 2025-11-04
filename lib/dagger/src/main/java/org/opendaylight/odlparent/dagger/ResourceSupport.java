2675f14c3f9680ad75fc1e735ec026341331a06f
Robert Varga <robert.varga@pantheon.tech>
2025-11-05 00:47:55 +0100

    Add lib/dagger(-example)
    This adds a dagger-specific module to aid with resource cleanup. We also
    include an example of a full OSGi/Dagger bundle, along with a test
    feature.
    
    JIRA: ODLPARENT-282
    Change-Id: Ic426be53962f22e36af78cfdbe338dd042d61c2d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ lib/dagger/src/main/java/org/opendaylight/odlparent/dagger/ResourceSupport.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.dagger;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * A registry for {@link AutoCloseable} resources to close when a component is shut down. This class should be used via
+ * {@link ResourceSupportModule}.
+ */
+@NonNullByDefault
+public abstract sealed class ResourceSupport permits DefaultResourceSupport {
+    /**
+     * Default constructor.
+     */
+    ResourceSupport() {
+        // Hidden onm purpose
+    }
+
+    /**
+     * Register an {@link AutoCloseable} resource with this registry.
+     *
+     * @param <T> resource type
+     * @param resource the resource
+     * @return the resource
+     * @throws IllegalStateException if this instance has been closed
+     */
+    public abstract <T extends AutoCloseable> T register(T resource);
+
+    /**
+     * Close this instance, calling {@link AutoCloseable#close()} on all registered resource in the reverse order
+     * of registration.
+     */
+    abstract void close();
+
+    @Override
+    public abstract String toString();
+}
---
+++ lib/dagger/src/test/java/org/opendaylight/odlparent/dagger/ResourceSupportTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.odlparent.dagger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mockingDetails;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ResourceSupportTest {
+    @Mock
+    private AutoCloseable one;
+    @Mock
+    private AutoCloseable two;
+
+    private final ResourceSupport registry = ResourceSupportModule.provideResourceSupport();
+
+    @Test
+    void toStringExposesState() {
+        assertThat(registry.toString()).startsWith("ResourceSupport [uuid=").endsWith(" state=Open]");
+        registry.close();
+        assertThat(registry.toString()).startsWith("ResourceSupport [uuid=").endsWith(" state=Closed]");
+    }
+
+    @Test
+    void closeOrdersRegistrants() throws Exception {
+        assertSame(one, registry.register(one));
+        assertSame(two, registry.register(two));
+
+        doNothing().when(one).close();
+        doThrow(Exception.class).when(two).close();
+
+        registry.close();
+
+        final var inOrder = inOrder(one, two);
+        inOrder.verify(two).close();
+        inOrder.verify(one).close();
+    }
+
+    @Test
+    void registerRejectedAfterClose() {
+        registry.close();
+        final var ex = assertThrows(IllegalStateException.class, () -> registry.register(one));
+        assertEquals("Cannot register one while closed", ex.getMessage());
+    }
+
+    @Test
+    void closeIsIdempotent() throws Exception {
+        doNothing().when(one).close();
+        assertSame(one, registry.register(one));
+        registry.close();
+        verify(one).close();
+        registry.close();
+        assertThat(mockingDetails(one).getInvocations()).hasSize(1);
+    }
+}
