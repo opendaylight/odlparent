16712332fbf0fca0e0c7ef6ed36f349edcc6f93f
Robert Varga <robert.varga@pantheon.tech>
2025-11-05 00:47:55 +0100

    Add lib/dagger(-example)
    This adds a dagger-specific module to aid with resource cleanup. We also
    include an example of a full OSGi/Dagger bundle, along with a test
    feature.
    
    JIRA: ODLPARENT-282
    Change-Id: Ic426be53962f22e36af78cfdbe338dd042d61c2d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ lib/dagger/src/main/java/module-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2025 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ * Common modules for {@code Dagger} integration.
+ */
+module org.opendaylight.odlparent.dagger {
+    exports org.opendaylight.odlparent.dagger;
+
+    requires transitive dagger;
+    requires transitive jakarta.inject;
+    requires transitive java.compiler;
+    requires com.google.errorprone.annotations;
+    requires org.slf4j;
+
+    // Annotations
+    requires static transitive org.eclipse.jdt.annotation;
+    // Workaround for Eclipse failing
+    requires static javax.inject;
+}
